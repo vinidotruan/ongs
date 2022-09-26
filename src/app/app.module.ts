26df65cd34123b6fd7d71c2a124bc90fa0328804
vinidotruan <contatoruanvinicius@gmail.com>
2022-09-26 17:27:42 -0300

    - w.i.p

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LogoComponent } from './shared/components/logo/logo.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, LogoComponent, SignUpComponent],
+  declarations: [AppComponent, LoginComponent, LogoComponent, SignUpComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
