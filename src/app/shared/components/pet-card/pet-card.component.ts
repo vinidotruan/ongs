b81d3f9588b4f47fc7086d55de7aa0fd77f7e912
vinidotruan <contatoruanvinicius@gmail.com>
2022-12-06 13:37:32 -0300

    - Fix redirect on pet card  - Fix float button css

--- src/app/shared/components/pet-card/pet-card.component.ts
+++ src/app/shared/components/pet-card/pet-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pet } from '@models/pet';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-pet-card',
@@ -9,7 +10,11 @@ import { Pet } from '@models/pet';
 export class PetCardComponent implements OnInit {
   @Input() pet: Pet;
   public dogUrl = 'assets/images/dog.png';
-  constructor() {}
+
+  constructor(private deviceSerivce: DeviceDetectorService) {}
 
   ngOnInit(): void {}
+
+  public getUrl = (): string =>
+    this.deviceSerivce.isDesktop() ? '/desktop' : '/mobile';
 }
